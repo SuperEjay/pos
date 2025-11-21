ace437eb21e1acbff36bd76a9247913b8fe32027
Earnest John Gungon <ej.gungon@springboardph.com>
2025-11-21 16:44:06 +0800

    Add routing configuration and redirect for orders
    - Introduced a new `vercel.json` file for routing rewrites to serve the application correctly.
    - Updated the main route in `index.tsx` to redirect users to the `/orders` path before loading the component, enhancing user navigation.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
+  beforeLoad: () => {
+    throw redirect({
+      to: '/orders',
+    })
+  },
 })
-
-function RouteComponent() {
-  return <div></div>
-}
